8773bf71a40ff39986028db6f0c3fad922905ce4
Moussa Sidibe <moussa.sidibe@ft.com>
2016-03-17 14:36:11 +0000

    AV-21 - coreftco module added

--- assanka/static/fckeditor/v2.5.1/editor/filemanager/browser/mcpuk/js/common.php
+++
@@ -1,8 +0,0 @@
-<?php
-header("Cache-Control: must-revalidate");
-header("Content-type: text/javascript");
-$offset = 60 * 60 * 24 * 3;
-header("Expires: " . gmdate("D, d M Y H:i:s", time() + $offset) . " GMT");
-
-include("common.js");
-?>
--- assanka/web/fckeditor/v2.5/fckcustomconfig.php
+++
@@ -1,6 +0,0 @@
-<?php
-	header("Content-Type: text/javascript");
-	
-	// If there are any project-specific files, this is where to insert them
-	if (file_exists($_SERVER["DOCUMENT_ROOT"]."/lib/inc/fckconfig.js")) include($_SERVER["DOCUMENT_ROOT"]."/lib/inc/fckconfig.js");
-?>
