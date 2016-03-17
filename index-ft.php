15a0b29714ac78d4ec1017f8b83c3a45b8a08186
Moussa Sidibe <moussa.sidibe@ft.com>
2016-03-17 15:00:21 +0000

    AV-21 - vendor dir re-installed

--- index-ft.php
+++ index-ft.php
@@ -5,7 +5,6 @@
  * @codingstandard ftlabs-phpcs
  * @copyright The Financial Times Limited [All rights reserved]
  */
-
 //set CORE_PATH for heroku
 //if(!isset($_SERVER['CORE_PATH'])) $_SERVER['CORE_PATH'] = $_SERVER['DOCUMENT_ROOT'].'/coreftco';
 
