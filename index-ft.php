8dfb07538841427dc1ac3841ce4467aef57b7f4b
Moussa Sidibe <moussa.sidibe@ft.com>
2016-03-22 17:02:40 +0000

    AV-21 - removed vendor include

--- index-ft.php
+++ index-ft.php
@@ -10,7 +10,7 @@ if(!isset($_SERVER['CORE_PATH'])) $_SERVER['CORE_PATH'] = $_SERVER['DOCUMENT_ROO
 
 //require_once(__DIR__ . '/vendor/autoload.php');
 //require_once $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
-
+/*
 $dir = $_SERVER['DOCUMENT_ROOT'] . "/vendor";
 if(!is_dir($dir)){
   echo 'is not a dir '.$dir;die;
@@ -25,16 +25,14 @@ foreach ($dir_list as $dir) {
     foreach (glob($dir."*.php") as $filename)
         require_once $filename;
 }
-
+*/
 
 //include_all_php($_SERVER['DOCUMENT_ROOT'] . "/vendor");
 
-if(!class_exists('FTLabs\Logger')){
-die($_SERVER['DOCUMENT_ROOT'].'/vendor');
-}
+
 // Initialise the FTLabs logger for nicer dev errors, but disable reporting to the Labs system
-$logger = FTLabs\Logger::init();
-$logger->setHandlerMinSeverity('report', \Psr\Log\LogLevel::EMERGENCY);
+i//$logger = FTLabs\Logger::init();
+//$logger->setHandlerMinSeverity('report', \Psr\Log\LogLevel::EMERGENCY);
 
 require_once $_SERVER['CORE_PATH']."/helpers/common/v2/common";
 
