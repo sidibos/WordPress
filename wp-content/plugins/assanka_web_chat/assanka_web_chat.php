943d8dbadb928354129ac4bc7151ca042662c748
Moussa Sidibe <moussa.sidibe@ft.com>
2016-03-23 15:06:04 +0000

    AV-21 - COREFTCO path added in wp-config

--- wp-content/plugins/assanka_web_chat/assanka_web_chat.php
+++ wp-content/plugins/assanka_web_chat/assanka_web_chat.php
@@ -8,7 +8,7 @@ Version: 1.0
 Author URI: http://assanka.net/
 */
 
-require_once $_SERVER['COREFTCO'].'/helpers/cacheability/cacheability';
+require_once COREFTCO.'/helpers/cacheability/cacheability';
 require_once dirname(__FILE__).'/assanka_web_chat_emoticon.php';
 
 class Assanka_Webchat {
