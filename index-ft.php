0ade04ce96950f6dd9f2009cfd131a3ee079b9cd
Moussa Sidibe <moussa.sidibe@ft.com>
2016-03-24 15:06:22 +0000

    AV-21 - added autoload func

--- index-ft.php
+++ index-ft.php
@@ -8,6 +8,7 @@
 
 //require_once(__DIR__ . '/vendor/autoload.php');
 require_once $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
+require_once(__DIR__.'/load-dependency.php');
 
 
 
---
+++ load-dependency.php
@@ -0,0 +1,26 @@
+<?php
+
+$vendor = require_once(__DIR__.'/vendor');
+
+$dependencies = [
+	$vendor.'/ftlabs/logger/src/FTLabs',
+	$vendor.'/ftlabs/helpers/src',
+	$vendor.'/ftlabs/memcache/src'
+];
+
+foreach($dependencies as $folder){
+	include_all_php($folder);
+}
+
+
+function include_all_php($folder){
+	foreach (glob("{$folder}/*.php") as $filename)
+	{
+		if(file_exists($filename)){
+			require_once $filename;
+		} else {
+			echo 'file do not exists '.$filename; die;
+		}
+	}
+}
+
