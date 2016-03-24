54728402c30095fe194a99f6e3dd0f6c7edca0a2
Moussa Sidibe <moussa.sidibe@ft.com>
2016-03-24 11:57:44 +0000

    AV-21 - logger removed from index-ft

--- index-ft.php
+++ index-ft.php
@@ -6,14 +6,14 @@
  * @copyright The Financial Times Limited [All rights reserved]
  */
 
-require_once(__DIR__ . '/vendor/autoload.php');
-//require_once $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
+//require_once(__DIR__ . '/vendor/autoload.php');
+require_once $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
 
 
 
 // Initialise the FTLabs logger for nicer dev errors, but disable reporting to the Labs system
-$logger = FTLabs\Logger::init();
-$logger->setHandlerMinSeverity('report', \Psr\Log\LogLevel::EMERGENCY);
+//$logger = FTLabs\Logger::init();
+//$logger->setHandlerMinSeverity('report', \Psr\Log\LogLevel::EMERGENCY);
 
 
 error_reporting(error_reporting() ^ E_STRICT ^ E_DEPRECATED ^ E_NOTICE);
