3419a9f06aca23756260fe1a63ddca5fd25d238c
Moussa Sidibe <moussa.sidibe@ft.com>
2016-04-04 16:12:08 +0100

    AV-21 - using wildcard when there is no HTTP_ORIGIN

--- wp-content/plugins/assanka_web_chat/assanka_web_chat.php
+++ wp-content/plugins/assanka_web_chat/assanka_web_chat.php
@@ -542,10 +542,12 @@ class Assanka_Webchat {
 			header('Access-Control-Allow-Methods: POST, GET, PUT, DELETE');
 			header('Access-Control-Allow-Headers: X-Requested-With, Content-Type');
 		}
-		else {
-			header('Access-Control-Allow-Origin: '.$_SERVER['HTTP_ORIGIN']);
-			//header('Access-Control-Allow-Origin: http://av2-wordpress-test.herokuapp.com/');
 
+		if(strlen($_SERVER['HTTP_ORIGIN'])) {
+			header('Access-Control-Allow-Origin: '.$_SERVER['HTTP_ORIGIN']);
+		}
+		else {
+			header('Access-Control-Allow-Origin: *');
 		}
 
 		//allows cookies (or other user credentials) to be included on cross-origin requests
