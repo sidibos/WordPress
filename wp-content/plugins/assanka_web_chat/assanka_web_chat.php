34661bd27a565b3772c92b9ff5649365dbe1faba
Moussa Sidibe <moussa.sidibe@ft.com>
2016-04-04 12:19:18 +0100

    AV-21 - use HTTP_ORIGIN instead of wildcard

--- wp-content/plugins/assanka_web_chat/assanka_web_chat.php
+++ wp-content/plugins/assanka_web_chat/assanka_web_chat.php
@@ -537,17 +537,17 @@ class Assanka_Webchat {
 
 		header('Vary: Origin');
 		if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-			header('Access-Control-Allow-Origin: *');
+			header('Access-Control-Allow-Origin: '.$_SERVER['HTTP_ORIGIN']);
 			//this allows preflights headers to work
 			header('Access-Control-Allow-Methods: POST, GET, PUT, DELETE');
 			header('Access-Control-Allow-Headers: X-Requested-With, Content-Type');
 		}
 		else {
-			header('Access-Control-Allow-Origin: *');
+			header('Access-Control-Allow-Origin: '.$_SERVER['HTTP_ORIGIN']);
 		}
 
 		//allows cookies (or other user credentials) to be included on cross-origin requests
-		//header('Access-Control-Allow-Credentials: true');
+		header('Access-Control-Allow-Credentials: true');
 
 		header("Content-Type: application/json");
 		header("Content-Length: ".strlen($op));
