51a5fa8d3cf19577837140b7a391ebf59d37b6e4
Moussa Sidibe <moussa.sidibe@ft.com>
2016-04-08 10:57:18 +0100

    AV-21 - adjuste sql to pgsql

--- wp-content/plugins/assanka_web_chat/assanka_web_chat.php
+++ wp-content/plugins/assanka_web_chat/assanka_web_chat.php
@@ -840,8 +840,8 @@ class Assanka_Webchat {
 		$channel = $this->getPusherChannel();
 		$ins_q = "INSERT INTO ".$wpdb->prefix."webchat_pusher (channel, event, data, datepushed_gmt)".
 				" VALUES ('".$channel."','".$event."','".json_encode($data,JSON_FORCE_OBJECT)."','".$now->format('Y-m-d H:i:s')."')";
-		$ins_q = stripslashes($ins_q);
-		$ins_q = str_replace("'", "''",$ins_q);
+		
+		$ins_q = str_replace("\\", "",$ins_q);
 
 
 		return array(
